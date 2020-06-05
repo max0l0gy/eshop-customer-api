0521cced1e17d3252a42cc42ccdeab3a1a1377a4
Maxim Morev <maxmorev@gmail.com>
2020-06-05 20:37:14 +0300

    feat(*): fixed response status code

--- src/main/java/ru/maxmorev/eshop/customer/api/rest/exception/GlobalDefaultExceptionHandler.java
+++ src/main/java/ru/maxmorev/eshop/customer/api/rest/exception/GlobalDefaultExceptionHandler.java
@@ -28,7 +28,7 @@ public class GlobalDefaultExceptionHandler {
      * @param ex
      * @return
      */
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Message handleBadRequest(HttpServletRequest req, Exception ex) {
